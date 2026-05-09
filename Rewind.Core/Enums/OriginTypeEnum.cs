9056c12a086e3f24ef1ff8f74abef84cff7b326d
Keven Grillo <kevengrillo@gmail.com>
2026-05-09 19:34:59 -0300

    Implementação no enum OriginTypeEnum.cs

--- Rewind.Core/Enums/OriginTypeEnum.cs
+++ Rewind.Core/Enums/OriginTypeEnum.cs
@@ -2,5 +2,10 @@ namespace HackathonEquipe6.Core.Enums;
 
 public enum OriginTypeEnum
 {
-    
+    Solar = 0,
+    Eolica = 1,
+    Hidrica = 2,
+    Fossil = 3,
+    Biomassa = 4,
+    Nuclear = 5
 }
