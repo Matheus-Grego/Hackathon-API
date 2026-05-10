656a1204e011f90581707f116564249874cea538
Matheus Grego <matheusgrego6@gmail.com>
2026-05-10 17:37:36 -0300

    fix - removendo campos

--- ReWind.Application/Models/ParkViewModel.cs
+++ ReWind.Application/Models/ParkViewModel.cs
@@ -1,4 +1,5 @@
 using HackathonEquipe6.Core.Entities;
+using HackathonEquipe6.Core.Enums;
 
 namespace HackathonEquipe6.Application.Models;
 
@@ -6,21 +7,11 @@ public class ParkViewModel
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string? ZipCode { get; set; }
-    public string? Phone { get; set; }
-    public string? Email { get; set; }
-    
-    public string DocumentNumber { get; set; }
-    public string Owner { get; set; }
-    public DateTime? OperationStartDate { get; set; }
     public decimal AuthorizedPowerKw { get; set; }
     public decimal InspectedPowerKw { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    
+    public CurtailmentRiskLevel? CurtailmentRiskLevel { get; set; }    
     public virtual List<ParkWaste> ParkWaste { get; set; }
     
     
@@ -30,20 +21,12 @@ public class ParkViewModel
         {
             Id = entity.Id,
             Name = entity.Name,
-            Address = entity.Address,
-            City = entity.City,
-            State = entity.State,
-            ZipCode = entity.ZipCode,
-            Phone = entity.Phone,
-            Email = entity.Email,
-            DocumentNumber = entity.DocumentNumber,
-            Owner = entity.Owner,
-            OperationStartDate = entity.OperationStartDate,
             AuthorizedPowerKw = entity.AuthorizedPowerKw,
             InspectedPowerKw = entity.InspectedPowerKw,
             Latitude = entity.Latitude,
             Longitude = entity.Longitude,
-            ParkWaste = entity.ParkWaste
+            ParkWaste = entity.ParkWaste,
+            CurtailmentRiskLevel =  entity.CurtailmentRiskLevel,
         };
     }
 
@@ -52,21 +35,12 @@ public class ParkViewModel
         return new Park
         {
             Name = this.Name,
-            Address = this.Address,
-            City = this.City,
-            State = this.State,
-            ZipCode = this.ZipCode,
-            Phone = this.Phone,
-            Email = this.Email,
-            Password = password,
-            DocumentNumber = this.DocumentNumber,
-            Owner = this.Owner,
-            OperationStartDate = this.OperationStartDate,
             AuthorizedPowerKw = this.AuthorizedPowerKw,
             InspectedPowerKw = this.InspectedPowerKw,
             Latitude = this.Latitude,
             Longitude = this.Longitude,
             ParkWaste = this.ParkWaste
+            
         };
     }
 
